714c0db025987927351c630c0cd5079f5cbbcdae
Sam <myworkplaceservice@gmail.com>
2021-02-23 22:24:49 +0530

    EditPad.com V1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./app.css";
 
 //function
 function Userwelcome(){
-    return <h1>Hello World</h1>;
+    //JSX to style like in h1
+    return (
+        //alway write code within div
+        <div>
+        <h1 style={{textAlign:'center',color:'white'}}>Editpad.com</h1>
+        <span className="txt"><textarea></textarea></span>
+        
+        <button className="button">Save</button>
+        
+        </div>
+      
+    );
 }
 
 
